0fa62de6fff6e515487102e50fe9048d790915cb
s3xmaxman <badside3919@gmail.com>
2024-07-02 06:14:37 +0900

    update fullstack_python/blog/__init__.py and fullstack_python.py

--- fullstack_python/blog/__init__.py
+++ fullstack_python/blog/__init__.py
@@ -1,4 +1,6 @@
 from .model import BlogPostModel
+from .list import blog_post_list_page
+from .state import BlogPostState
 
 
-__all__ = ["BlogPostModel"]
+__all__ = ["BlogPostModel", "blog_post_list_page", "BlogPostState"]
--- fullstack_python/fullstack_python.py
+++ fullstack_python/fullstack_python.py
@@ -50,9 +50,23 @@ def index() -> rx.Component:
 
 app = rx.App()
 app.add_page(index)
-app.add_page(pages.about_page, route=navigation.routes.ABOUT_US_ROUTE)
-app.add_page(pages.pricing_page, route=navigation.routes.PRICING_ROUTE)
-app.add_page(contact.contact_page, route=navigation.routes.CONTACT_US_ROUTE)
+app.add_page(
+    pages.about_page,
+    route=navigation.routes.ABOUT_US_ROUTE,
+)
+app.add_page(
+    blog.blog_post_list_page,
+    route=navigation.routes.BLOG_POSTS_ROUTE,
+    on_load=blog.BlogPostState.load_posts,
+)
+app.add_page(
+    pages.pricing_page,
+    route=navigation.routes.PRICING_ROUTE,
+)
+app.add_page(
+    contact.contact_page,
+    route=navigation.routes.CONTACT_US_ROUTE,
+)
 app.add_page(
     contact.contact_entries_list_page,
     route=navigation.routes.CONTACT_ENTRIES_ROUTE,
