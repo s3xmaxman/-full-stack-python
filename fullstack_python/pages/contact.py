b6eb9a0ed3df7adf7b622d52fd919f8ceda2b1e5
s3xmaxman <badside3919@gmail.com>
2024-07-01 13:39:25 +0900

    update fullstack_python.py, contact.py and pricing.py

--- fullstack_python/pages/contact.py
+++ fullstack_python/pages/contact.py
@@ -2,10 +2,57 @@ import reflex as rx
 from ..ui.base import base_page
 
 
+import reflex as rx
+from ..ui.base import base_page
+
+
+class ContactState(rx.State):
+    from_data: dict = {}
+
+    def handle_submit(self, from_data: dict):
+        print(from_data)
+        self.from_data = from_data
+
+
 def contact_page() -> rx.Component:
+    my_form = (
+        rx.form(
+            rx.vstack(
+                rx.hstack(
+                    rx.input(
+                        placeholder="First Name",
+                        name="first_name",
+                        required=True,
+                        width="100%",
+                    ),
+                    rx.input(
+                        placeholder="Last Name",
+                        name="last_name",
+                        width="100%",
+                    ),
+                ),
+                rx.input(
+                    placeholder="Your Email",
+                    name="email",
+                    type="email",
+                    width="100%",
+                ),
+                rx.text_area(
+                    placeholder="Your Message",
+                    name="message",
+                    required=True,
+                    width="100%",
+                ),
+                rx.button("Submit", type="submit"),
+            ),
+            on_submit=ContactState.handle_submit,
+            reset_on_submit=True,
+        ),
+    )
     my_child = rx.vstack(
         rx.heading("Contact", size="9"),
-        rx.text("Something about Reflex"),
+        rx.mobile_and_tablet(rx.box(my_form, width="85vw")),
+        rx.desktop_only(rx.box(my_form, width="50vw")),
         spacing="5",
         justify="center",
         min_height="85vh",
--- fullstack_python/pages/pricing.py
+++ fullstack_python/pages/pricing.py
@@ -2,46 +2,10 @@ import reflex as rx
 from ..ui.base import base_page
 
 
-class ContactState(rx.State):
-    from_data: dict = {}
-
-    def handle_submit(self, from_data: dict):
-        print(from_data)
-        self.from_data = from_data
-
-
 def pricing_page() -> rx.Component:
-    my_form = (
-        rx.form(
-            rx.vstack(
-                rx.input(
-                    placeholder="First Name",
-                    name="first_name",
-                    required=True,
-                ),
-                rx.input(
-                    placeholder="Last Name",
-                    name="last_name",
-                ),
-                rx.input(
-                    placeholder="Your Email",
-                    name="email",
-                    type="email",
-                ),
-                rx.text_area(
-                    placeholder="Your Message",
-                    name="message",
-                    required=True,
-                ),
-                rx.button("Submit", type="submit"),
-            ),
-            on_submit=ContactState.handle_submit,
-            reset_on_submit=True,
-        ),
-    )
     my_child = rx.vstack(
-        rx.heading("Pricing", size="9"),
-        my_form,
+        rx.heading("About", size="9"),
+        rx.text("Something about Reflex"),
         spacing="5",
         justify="center",
         min_height="85vh",
