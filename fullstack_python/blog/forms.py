131e99b5a216dcaabdee3a9e0355929a3d518b6e
s3xmaxman <badside3919@gmail.com>
2024-07-03 02:43:33 +0900

    update edit.py, forms.py and state.py

--- fullstack_python/blog/forms.py
+++ fullstack_python/blog/forms.py
@@ -1,10 +1,7 @@
 import reflex as rx
 
 
-from .state import (
-    BlogAddPostFormState,
-    # BlogEditFormState
-)
+from .state import BlogAddPostFormState, BlogEditFormState
 
 
 def blog_post_add_form() -> rx.Component:
@@ -31,4 +28,45 @@ def blog_post_add_form() -> rx.Component:
         ),
         on_submit=BlogAddPostFormState.handle_submit,
         reset_on_submit=True,
+    )
+
+
+def blog_post_edit_form() -> rx.Component:
+    post = BlogEditFormState.post
+    title = post.title
+    post_content = BlogEditFormState.post_content
+    return rx.form(
+        rx.box(
+            rx.input(
+                type="hidden",
+                name="post_id",
+                value=post.id,
+            ),
+            display="none",
+        ),
+        rx.vstack(
+            rx.hstack(
+                rx.input(
+                    default_value=title,
+                    on_change=BlogEditFormState.set_title,
+                    name="title",
+                    placeholder="Title",
+                    required=True,
+                    type="text",
+                    width="100%",
+                ),
+                width="100%",
+            ),
+            rx.text_area(
+                value=post_content,
+                on_change=BlogEditFormState.set_post_content,
+                name="content",
+                placeholder="Your message",
+                required=True,
+                height="50vh",
+                width="100%",
+            ),
+            rx.button("Submit", type="submit"),
+        ),
+        on_submit=BlogEditFormState.handle_submit,
     )
--- fullstack_python/blog/state.py
+++ fullstack_python/blog/state.py
@@ -7,6 +7,7 @@ from .model import BlogPostModel
 class BlogPostState(rx.State):
     posts: List["BlogPostModel"] = []
     post: Optional["BlogPostModel"] = None
+    post_content: str = ""
 
     @rx.var
     def blog_post_id(self):
@@ -24,10 +25,29 @@ class BlogPostState(rx.State):
             ).one_or_none()
 
             self.post = result
+            self.post_content = result.content
 
     def add_post(self, form_data: dict):
         with rx.session() as session:
             post = BlogPostModel(**form_data)
+            session.add(post)
+            session.commit()
+            session.refresh(post)
+            self.post = post
+
+    def edit_post(self, post_id: int, updated_data: dict):
+        with rx.session() as session:
+            post = session.exec(
+                select(BlogPostModel).where(BlogPostModel.id == self.blog_post_id)
+            ).one_or_none()
+            post = BlogPostModel(**updated_data)
+
+            if post is None:
+                return
+
+            for key, value in updated_data.items():
+                setattr(post, key, value)
+
             session.add(post)
             session.commit()
             session.refresh(post)
@@ -43,3 +63,14 @@ class BlogAddPostFormState(BlogPostState):
     def handle_submit(self, from_data):
         self.form_data = from_data
         self.add_post(from_data)
+
+
+class BlogEditFormState(BlogPostState):
+    form_data: dict = {}
+    content: str = ""
+
+    def handle_submit(self, from_data):
+        self.form_data = from_data
+        post_id = from_data.pop("post_id")
+        updated_data = {**from_data}
+        self.edit_post(post_id, updated_data)
